2f56a632b19a60f28d0a2025d526b1a827cc6721
marceloarauj <42809145+marceloarauj@users.noreply.github.com>
2020-01-03 23:27:48 -0300

    Ajustes no visual

--- src/app/library/header/header.component.ts
+++ src/app/library/header/header.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { MatIconRegistry } from '@angular/material/icon';
-import { DomSanitizer } from '@angular/platform-browser';
 import {MenuComponent} from '../menu/menu.component';
 import {BehaviorSubject} from 'rxjs';
 @Component({
@@ -12,15 +10,7 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit() {}
 
-  constructor(private matIconRegistry: MatIconRegistry,private domSanitizer: DomSanitizer) {
-    //icone do usuario perfil  
-    this.matIconRegistry.addSvgIcon('perfil',
-          this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/perfil_icone.svg'));
-    
-    //icone de ajuda 
-    this.matIconRegistry.addSvgIcon('ajuda',
-          this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/ajuda_icone.svg'));
-    }
+  constructor() {}
 
     ComportamentoDrawer = new BehaviorSubject<any>(false);
 
